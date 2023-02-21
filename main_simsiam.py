0ec9eb96acb1a2f4575ae98dc64ae941746e2975
ferreirafabio <ferreirafabio@users.noreply.github.com>
2023-02-21 11:45:25 +0100

    implemented support for stn_warmup_epochs

--- main_simsiam.py
+++ main_simsiam.py
@@ -679,7 +679,7 @@ def train(train_loader, model, criterion, optimizer, stn_optimizer, stn, target_
         # compute output and loss
         penalty_l = 0
         if args.use_stn:
-            penalty_l = torch.exp(torch.tensor(penalty * args.penalty_weight))
+            penalty_l = torch.exp(penalty * args.penalty_weight)
         
         if args.four_way_loss:
             p1, p2, z1, z2 = model(x1=images, x2=stn_images[0])
